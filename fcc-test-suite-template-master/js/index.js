28d3d16638e42226795f6bed178ec8b1df1c72f6
RezaAnsari83 <rezaansarii760@gmail.com>
2023-05-11 10:47:05 -0700

    third commit
    created html code also css code

--- fcc-test-suite-template-master/js/index.js
+++ fcc-test-suite-template-master/js/index.js
@@ -25,4 +25,93 @@ INSTRUCTIONS:
 // PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
 
 // Once you have read the above messages, you can delete all comments. 
+Skip to content
+Product
+Solutions
+Open Source
+Pricing
+Search
+Sign in
+Sign up
+FreeCodeCamp-Solutions
+/
+Build-A-Tribute-Page
+Public
+Code
+Issues
+1
+Pull requests
+1
+Actions
+Projects
+Security
+Insights
+Build-A-Tribute-Page/css/style.css
+@AfifOne
+AfifOne Tribute to Steve Jobs
+Latest commit 5216e09 on Aug 28, 2020
+ History
+ 1 contributor
+44 lines (32 sloc)  510 Bytes
+ 
+
+#title {
+    font-size: 400%;
+    width: 60vmax;
+  }
+
+.center {
+    margin: auto;
+    padding: auto;
+    text-align: center;
+  }
+
+#image {
+    max-width: 70%;
+    height: auto;
+    display: block;
+  }
+
+
+  @media only screen and (min-width:480px)
+  {
+
+  }
+
+  @media only screen and (min-width:767px)
+  {
+
+  }
+
+  @media only screen and (min-width:991px)
+  {
+
+  }
+
+  @media only screen and (min-width:1200px)
+  {
+ .font {
+    padding: 0px 200px 15px 200px;
+        }
+  }
+
+  @media(orientation: landscape)
+  {
+
+  }
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Build-A-Tribute-Page/style.css at master · FreeCodeCamp-Solutions/Build-A-Tribute-Page · GitHub
 "use strict";
